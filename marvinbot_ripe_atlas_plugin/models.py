b80813f62027ef66862b0a2c43264208f94d19db
Ricardo Arturo Cabral <ricardo.arturo.cabral@gmail.com>
2016-11-16 23:32:10 -0400

    Added /probe_alias command

--- marvinbot_ripe_atlas_plugin/models.py
+++ marvinbot_ripe_atlas_plugin/models.py
@@ -1,1 +1,45 @@
-# TODO Add model for named probes
+import mongoengine
+from marvinbot.utils import localized_date
+
+
+class ProbeAlias(mongoengine.Document):
+    id = mongoengine.SequenceField(primary_key=True)
+    alias = mongoengine.StringField(unique=True)
+    probe_id = mongoengine.LongField(required=True)
+    user_id = mongoengine.LongField(required=True)
+    username = mongoengine.StringField(required=True)
+
+    date_added = mongoengine.DateTimeField(default=localized_date)
+    date_modified = mongoengine.DateTimeField(default=localized_date)
+    date_deleted = mongoengine.DateTimeField(required=False, null=True)
+
+    @classmethod
+    def by_id(cls, id):
+        try:
+            return cls.objects.get(id=id)
+        except cls.DoesNotExist:
+            return None
+
+    @classmethod
+    def by_probe_id(cls, probe_id):
+        try:
+            return cls.objects.get(probe_id=probe_id)
+        except cls.DoesNotExist:
+            return None
+
+    @classmethod
+    def by_alias(cls, alias):
+        try:
+            return cls.objects.get(alias=alias)
+        except cls.DoesNotExist:
+            return None
+
+    @classmethod
+    def all(cls):
+        try:
+            return cls.objects(date_deleted=None)
+        except:
+            return None
+
+    def __str__(self):
+        return "{{ id = {id}, alias = \"{alias}\", probe_id = \"{probe_id}\" }}".format(id=self.id, probe_id=self.probe_id, alias=self.alias)
--- marvinbot_ripe_atlas_plugin/tasks.py
+++ marvinbot_ripe_atlas_plugin/tasks.py
@@ -1,5 +1,6 @@
-from marvinbot.utils import get_message
+from marvinbot.utils import get_message, localized_date
 from marvinbot.handlers import CommandHandler
+from marvinbot_ripe_atlas_plugin.models import ProbeAlias
 from marvinbot_ripe_atlas_plugin import get_config
 import logging
 import json
@@ -10,22 +11,119 @@ log = logging.getLogger(__name__)
 adapter = None
 
 
-def fetch_probe(probe_id, api_key):
+def remove_probe_alias(alias):
+    probe_alias = ProbeAlias.by_alias(alias)
+    if probe_alias and probe_alias.date_deleted is None:
+        probe_alias.date_deleted = localized_date()
+        try:
+            probe_alias.save()
+            return True
+        except:
+            return False
+    return False
+
+
+def query_probe_status(probe_id, api_key):
     base_url = 'https://atlas.ripe.net//api/v2/probes/{probe_id}/?key={api_key}'
-    response = requests.get(base_url.format(probe_id=probe_id, api_key=api_key))
-    data = response.json()
-    return data
+    try:
+        response = requests.get(base_url.format(probe_id=probe_id, api_key=api_key))
+        if response.ok:
+            data = response.json()
+            return data
+        else:
+            return None
+    except:
+        return None
+
+
+def on_probe_alias_command(update, *args, **kwargs):
+    log.info('on_probe_alias_command')
+    message = get_message(update)
+    probe_id = kwargs.get('probe_id')
+    alias = kwargs.get('alias')
+    remove = kwargs.get('remove')
+
+    if alias is None:
+        adapter.bot.sendMessage(chat_id=message.chat_id,
+                                text="❌ You must specify an alias.")
+        return
+
+    if len(alias) == 0:
+        adapter.bot.sendMessage(chat_id=message.chat_id,
+                                text="❌ Specified alias is too short")
+        return
+
+    if remove:
+        if remove_probe_alias(alias):
+            adapter.bot.sendMessage(chat_id=message.chat_id,
+                                    text="🚮 1 probe alias removed.")
+        else:
+            adapter.bot.sendMessage(chat_id=message.chat_id,
+                                    text="❌ No such probe alias.")
+        return
+
+    if probe_id is None:
+        adapter.bot.sendMessage(chat_id=message.chat_id,
+                                text="❌ You must specify a probe.")
+        return
+
+    if len(probe_id) == 0:
+        adapter.bot.sendMessage(chat_id=message.chat_id,
+                                text="❌ Specified probe ID is too short.")
+        return
+
+    probe_alias = ProbeAlias.by_alias(alias)
+
+    if probe_alias:
+        if probe_alias.date_deleted is None:
+            adapter.bot.sendMessage(chat_id=message.chat_id,
+                                    text="❌ Probe alias already exists.")
+            return
+
+    user_id = message.from_user.id
+    username = message.from_user.username
+    date_added = localized_date()
+    date_modified = localized_date()
+
+    if probe_alias is None:
+        probe_alias = ProbeAlias()
+
+    probe_alias.probe_id = probe_id
+    probe_alias.alias = alias
+    probe_alias.user_id = user_id
+    probe_alias.username = username
+    probe_alias.date_modified = date_modified
+    probe_alias.date_deleted = None
+
+    try:
+        probe_alias.save()
+        adapter.bot.sendMessage(chat_id=message.chat_id, text="✅ Probe alias added.")
+    except:
+        adapter.bot.sendMessage(chat_id=message.chat_id,
+                                text="❌ Unable to add probe alias.")
 
 
 def on_probe_command(update, *args, **kwargs):
-    log.info('Probe command caught')
     message = get_message(update)
     probes = kwargs.get('probe_id')
     api_key = get_config().get("api_key")
     responses = []
-    for probe_id in probes:
-        data = fetch_probe(probe_id, api_key)
-        log.info(json.dumps(data, indent=4, sort_keys=True))
+    for probe in probes:
+        if probe.isdigit():
+            probe_id = probe
+        else:
+            probe_alias = ProbeAlias.by_alias(probe)
+            if probe_alias is None or probe_alias.date_deleted is not None:
+                adapter.bot.sendMessage(chat_id=message.chat_id,
+                                        text="❌ Not a valid probe alias.")
+                return
+            else:
+                probe_id = probe_alias.probe_id
+        data = query_probe_status(probe_id, api_key)
+        if not data:
+            adapter.bot.sendMessage(chat_id=message.chat_id,
+                                    text="❌ Probe does not exist.")
+            continue
         if data.get("status") is None:
             continue
         status = data.get("status")
@@ -50,3 +148,7 @@ def setup(new_adapter):
 
     adapter.add_handler(CommandHandler('probe', on_probe_command, command_description='Allows the user to get status for a given probe.')
                         .add_argument('probe_id', nargs='*', help='Probe ID'))
+    adapter.add_handler(CommandHandler('probe_alias', on_probe_alias_command, command_description='Allows the user to set an alias for a given probe.')
+                        .add_argument('--remove', help='Remove alias', action='store_true')
+                        .add_argument('--alias', help='Set an alias (e.g. Santiago)')
+                        .add_argument('--probe_id', help='Probe ID (e.g. 11816)'))
